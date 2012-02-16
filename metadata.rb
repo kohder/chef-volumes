f20de52ccb224c5760abfae751a4e07d296efa62
Rob Lewis <rlewis@cargometrics.com>
2012-02-16 11:40:26 -0500

    Volumes recipe implementation.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'volumes'
-maintainer       "Rob Lewis <rob@kohder.com>"
+maintainer       "Rob Lewis"
 maintainer_email "rob@kohder.com"
 license          "Apache 2.0"
 description      "Installs/Configures volumes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+recipe           "volumes", "Configures disks/volumes"
+depends          "lvm"
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+all_volume_plan_names = data_bag('volume_plans')
+node_volume_plans = Array(node['volumes']['plans'])
+node_volume_plans.each do |node_volume_plan_name|
+  if !all_volume_plan_names.include?(node_volume_plan_name)
+    Chef::Log.error("No data bag entry for volume plan\"#{node_volume_plan_name}\"")
+    next
+  end
+  volume_plan = data_bag_item('volume_plans', node_volume_plan_name)
+
+  Chef::Log.info("Applying volume plan: #{node_volume_plan_name}")
+
+  volume_plan['lvm_volume_groups'].each do |lvm_volume_group|
+    physical_volumes = Array(lvm_volume_group['physical_volumes'])
+    unless physical_volumes.empty?
+
+      lvm_pv 'pvcreate' do
+        devices physical_volumes
+        action :create
+      end
+
+      lvm_vg 'vgcreate' do
+        devices physical_volumes
+        volume_group_name lvm_volume_group['name']
+        action :create
+      end
+
+      logical_volumes = Array(lvm_volume_group['logical_volumes'])
+      logical_volumes.each do |logical_volume|
+        filesystem_type = logical_volume['filesystem'] || 'xfs'
+        device_name = "/dev/mapper/#{lvm_volume_group['name']}-#{logical_volume['name']}"
+
+        execute "mkfs" do
+          command "yes | mkfs -t #{filesystem_type} #{logical_volume['filesystem_opts']} #{device_name}"
+          action :nothing
+        end
+
+        lvm_lv 'lvcreate' do
+          volume_group_name lvm_volume_group['name']
+          logical_volume_name logical_volume['name']
+          stripes logical_volume['stripes']
+          stripe_size logical_volume['stripe_size']
+          logical_extents logical_volume['logical_extents']
+          action :create
+          notifies :run, resources(:execute => "mkfs"), :immediately
+        end
+
+        mount_point = logical_volume['mount']
+        if mount_point
+          mount mount_point do
+            device device_name
+            fstype filesystem_type
+            options logical_volume['mount_opts']
+            action [:mount, :enable]
+          end
+
+          directory mount_point do
+            mode '0777'
+            action :create
+          end
+        end
+      end
+    end
+  end
+end
